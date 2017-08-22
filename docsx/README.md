a7ef34f4df4e73c5c5d52c3898f06c68eadb8667
Rasmus Andersson <rasmus@notion.se>
2017-08-22 11:53:36 -0700

    trying to fix broken github pages

---
+++ docsx/README.md
@@ -0,0 +1,1 @@
+This directory is published as a website by Github at [https://rsms.me/interface](https://rsms.me/interface/)
---
+++ docsx/serve.sh
@@ -0,0 +1,24 @@
+#!/bin/sh
+set -e
+cd "$(dirname "$0")"
+
+if [ ! -s lab/fonts ]; then
+  ln -s ../../build/dist lab/fonts
+fi
+
+if (which caddy >/dev/null); then
+  caddy_args=(\
+    -host localhost \
+    "bind localhost" \
+    "mime .woff2 font/woff2" \
+    "mime .woff application/font-woff" \
+  )
+  caddy "${caddy_args[@]}"
+elif (which servedir >/dev/null); then
+  servedir
+else
+  echo "Can not find 'caddy' nor 'servedir' in PATH." >&2
+  echo "Install caddy from brew, apt or https://caddyserver.com/download"
+  echo "or install servedir with 'npm install -g secure-servedir'"
+  exit 1
+fi
