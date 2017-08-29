77163d2973568b90acbd088c7b57f5f4d6703fbe
Rasmus Andersson <rasmus@notion.se>
2017-08-28 19:24:39 -0700

    website: improvements for safari

--- docs/index.js
+++ docs/index.js
@@ -4,7 +4,10 @@ if (!window.MSStream &&
     /mac|ipad|iphone|ipod/i.test(navigator.userAgent))
 {
   isMac = true
-  document.body.classList = 'mac_or_ios'
+  document.body.classList.add('mac_or_ios')
+  if (navigator.userAgent.indexOf('Safari') != -1) {
+    document.body.classList.add('safari')
+  }
 }
 
 (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
