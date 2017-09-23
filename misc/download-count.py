d457ac8e05be5c904704d819668859f12e21ec6d
Rasmus Andersson <rasmus@notion.se>
2017-09-23 10:12:51 -0700

    Fix download count script. Note that it only counts after the projects was renamed to "Inter"

--- misc/download-count.py
+++ misc/download-count.py
@@ -3,14 +3,17 @@
 from __future__ import print_function
 import os, sys, json, urllib2
 
-f = urllib2.urlopen('https://api.github.com/repos/rsms/interface/releases')
+f = urllib2.urlopen('https://api.github.com/repos/rsms/inter/releases')
 releases = json.load(f)
 
 countTotal = 0
 
 for release in releases:
-  count = release['assets'][0]['download_count']
-  countTotal += count
-  print('%s: %d' % (release['tag_name'], count))
+  if len(release['assets']) > 0:
+    count = release['assets'][0]['download_count']
+    countTotal += count
+    print('%s: %d' % (release['tag_name'], count))
+  else:
+    print('%s: (missing)' % release['tag_name'])
 
 print('Total: %d' % countTotal)
