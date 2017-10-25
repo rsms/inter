86cfd8c1f8442603c40d6e9223f8a2bf3d07dad1
Rasmus Andersson <rasmus@notion.se>
2017-10-25 11:08:27 -0700

    website: res optimization script updates

--- docs/res/optimize.sh
+++ docs/res/optimize.sh
@@ -11,4 +11,15 @@ for f in *.png; do
   (pngcrush -q "$f" "$TMPNAME" && mv -f "$TMPNAME" "$f") &
 done
 
+pushd sample >/dev/null
+for f in *.png; do
+  TMPNAME=.$f.tmp
+  if (echo "$f" | grep -q 'thumb'); then
+    (convert "$f" -flatten -background white -colors 16 "$TMPNAME" && pngcrush -q "$TMPNAME" "$f") &
+  else
+    (pngcrush -q "$f" "$TMPNAME" && mv -f "$TMPNAME" "$f") &
+  fi
+done
+popd >/dev/null
+
 wait
