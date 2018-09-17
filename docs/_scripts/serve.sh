724caef757f943e6350331b5371b2d13beefc2a8
Rasmus Andersson <rasmus@notion.se>
2018-09-17 10:35:12 -0700

    update website (note: the live website is sourced from the master branch, so these changes will not be visible on the actual website until this branch is merged into master, or if you run the server locally.)

--- docs/_scripts/serve.sh
+++ docs/_scripts/serve.sh
@@ -3,31 +3,27 @@ set -e
 cd "$(dirname "$0")/.."
 
 if [ ! -s lab/fonts ]; then
-  rm -rf lab/fonts
+  rm -f lab/fonts
   ln -fs ../../build/fonts lab/fonts
 fi
-
-rm -rf _site
 
 # jekyll is a little dumb and resolves the lab/fonts symlink and copies
 # all font files to _site when started. Bad jekyll.
 # Let's work around that.
+rm -rf _site
 sh <<_EOF_ &
 N=3
 while [ \$N -gt 0 ]; do
   sleep 1
   mkdir -p _site/lab
-  ln -fs ../../../build/fonts _site/lab/fonts
-  sleep 5
   if [ -d _site/lab/fonts ]; then
     rm -rf _site/lab/fonts
   else
     rm -f _site/lab/fonts
   fi
-  mkdir -p _site/lab
   ln -fs ../../../build/fonts _site/lab/fonts
   let N=N-1
 done
 _EOF_
 
-jekyll serve --limit_posts 20 --watch --host 127.0.0.1 --port 3002 --open-url
+jekyll serve --limit_posts 20 --watch --host 127.0.0.1 --port 3002
