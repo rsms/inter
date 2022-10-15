9d9bfc3eecdd7ee17fe36827c400c8dcc0eb4cd5
Rasmus Andersson <rasmus@notion.se>
2022-10-15 16:34:57 -0700

    workaround for bug in defcon

--- misc/tools/postprocess_instance_ufo.py
+++ misc/tools/postprocess_instance_ufo.py
@@ -19,7 +19,12 @@ def ufo_set_wws(ufo):
 # TODO: Remove when https://github.com/googlefonts/glyphsLib/issues/821 is fixed
 def fix_fractional_advance_width(ufo):
   for g in ufo:
-    g.width = round(g.width)
+    w = int(round(g.width))
+    # set twice to work around bug or weird behavior in defcon.
+    # If we don't do this, then fractional widths with .0 fraction are
+    # not updated to integer values.
+    g.width = w + 1
+    g.width = w
 
 
 def main(argv):
