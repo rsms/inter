3be775a4ebd35129bb8bdbdba2e07ac20e8e8d8a
Rasmus Andersson <rasmus@notion.se>
2023-06-04 16:57:11 -0700

    changes style names to remove spaces, i.e. "Extra Bold" -> "ExtraBold". This should improve life on MS Windows

--- misc/tools/gen-instance-ufo.sh
+++ misc/tools/gen-instance-ufo.sh
@@ -5,32 +5,32 @@ DESIGNSPACE=$1
 UFO=$2
 
 INSTANCE_Light="Inter Light"
-INSTANCE_ExtraLight="Inter Extra Light"
+INSTANCE_ExtraLight="Inter ExtraLight"
 INSTANCE_Medium="Inter Medium"
-INSTANCE_SemiBold="Inter Semi Bold"
+INSTANCE_SemiBold="Inter SemiBold"
 INSTANCE_Bold="Inter Bold"
-INSTANCE_ExtraBold="Inter Extra Bold"
+INSTANCE_ExtraBold="Inter ExtraBold"
 
 INSTANCE_DisplayLight="Inter Display Light"
-INSTANCE_DisplayExtraLight="Inter Display Extra Light"
+INSTANCE_DisplayExtraLight="Inter Display ExtraLight"
 INSTANCE_DisplayMedium="Inter Display Medium"
-INSTANCE_DisplaySemiBold="Inter Display Semi Bold"
+INSTANCE_DisplaySemiBold="Inter Display SemiBold"
 INSTANCE_DisplayBold="Inter Display Bold"
-INSTANCE_DisplayExtraBold="Inter Display Extra Bold"
+INSTANCE_DisplayExtraBold="Inter Display ExtraBold"
 
 INSTANCE_LightItalic="Inter Light Italic"
-INSTANCE_ExtraLightItalic="Inter Extra Light Italic"
+INSTANCE_ExtraLightItalic="Inter ExtraLight Italic"
 INSTANCE_MediumItalic="Inter Medium Italic"
-INSTANCE_SemiBoldItalic="Inter Semi Bold Italic"
+INSTANCE_SemiBoldItalic="Inter SemiBold Italic"
 INSTANCE_BoldItalic="Inter Bold Italic"
-INSTANCE_ExtraBoldItalic="Inter Extra Bold Italic"
+INSTANCE_ExtraBoldItalic="Inter ExtraBold Italic"
 
 INSTANCE_DisplayLightItalic="Inter Display Light Italic"
-INSTANCE_DisplayExtraLightItalic="Inter Display Extra Light Italic"
+INSTANCE_DisplayExtraLightItalic="Inter Display ExtraLight Italic"
 INSTANCE_DisplayMediumItalic="Inter Display Medium Italic"
-INSTANCE_DisplaySemiBoldItalic="Inter Display Semi Bold Italic"
+INSTANCE_DisplaySemiBoldItalic="Inter Display SemiBold Italic"
 INSTANCE_DisplayBoldItalic="Inter Display Bold Italic"
-INSTANCE_DisplayExtraBoldItalic="Inter Display Extra Bold Italic"
+INSTANCE_DisplayExtraBoldItalic="Inter Display ExtraBold Italic"
 
 MASTER_Thin=1
 MASTER_Regular=1
@@ -51,7 +51,7 @@ _err() { echo "$PROG: $@" >&2; exit 1; }
 UFO_NAME=$(basename "$UFO" .ufo)
 case "$UFO_NAME" in Inter-*) UFO_NAME=${UFO_NAME:6} ;; esac
 
-# DisplayExtraBold -> "Inter Display Extra Bold"
+# DisplayExtraBold -> "Inter Display ExtraBold"
 INSTANCE=INSTANCE_${UFO_NAME} ; INSTANCE=${!INSTANCE}
 
 # If UFO is not an instance, assume that it's a master (or else error)
